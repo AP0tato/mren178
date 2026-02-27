dfe5f2b36d5e90882aa9d663d21fa7d7078faa20
AP0tato <aliaid0207@gmail.com>
2026-02-27 11:36:45 -0500

    stuff

---
+++ lab3/lab03_1/SerialClass.h
@@ -0,0 +1,28 @@
+#ifndef SERIALCLASS_H_INCLUDED
+#define SERIALCLASS_H_INCLUDED
+
+#define ARDUINO_WAIT_TIME 2000
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+    //Initialize Serial communication with the given COM port
+    void Serial(const char *portName);
+    //Close the connection
+    void _Serial();
+    //Read data in a buffer, if nbChar is greater than the
+    //maximum number of bytes available, it will return only the
+    //bytes available. The function return -1 when nothing could
+    //be read, the number of bytes actually read.
+    int ReadData(char *buffer, unsigned int nbChar);
+    //Writes data from a buffer through the Serial connection
+    //return true on success.
+    int WriteData(const char *buffer, unsigned int nbChar);
+    //Check if we are actually connected
+    int IsConnected();
+    int DataRead();
+
+#endif // SERIALCLASS_H_INCLUDED
---
+++ lab3/lab03_1/lab3_main.c
@@ -0,0 +1,78 @@
+#include "lab3_bst.h"
+
+#define COMPORT "COM4" // adjust as needed
+#define NODEADDED 10
+#define DATAREADGAP 1000
+
+// The lab requires that you implement 3 functions:
+//	int height (Node *);
+//	Node* findParentHelper (Key, Node*);
+//	delete (Node*, Node*);
+
+#define	HEIGHT_WRITTEN	0
+#define	FINDPARENTHELPER_WRITTEN	0
+#define	DELETE_WRITTEN	0
+//
+// *** YOU SHOULD NOT MAKE ANY OTHER CHANGES TO THIS CODE ***
+
+int main(void)
+{
+	Node	*n = NULL;		// eliminates compiler warning
+	int v1 = 1;
+	Tree *t = initTree (50,&v1);//the tree will be starting with a initial node of 50
+
+	
+	printf("Connecting to serial port ");
+	printf(COMPORT);
+	printf("\n");
+
+	Serial(COMPORT);    
+	if (IsConnected())
+	printf("We're connected\n\n");
+
+	//keeping an array to keep track of the node values that have been stored
+	int numArray[NODEADDED];
+
+	for(int i=0;i<NODEADDED;i++){
+		numArray[i]=DataRead();
+		insert(numArray[i],&v1,t->root);
+		Sleep(DATAREADGAP);
+	}
+
+	printf("\n\n");
+	
+	printf("Original Tree: \n");
+	printTree(t->root);
+	printf("\n\n");
+
+	if (HEIGHT_WRITTEN == 1)	{
+		printf("Height of tree: %d\n\n", _height(t->root));
+		}
+
+	if (FINDPARENTHELPER_WRITTEN == 1)	{
+		printf("Parent of %d: %d\n",numArray[3], findParent(numArray[3],t->root)->key);
+		printf("Parent of %d: %d\n",numArray[6], findParent(numArray[6],t->root)->key);
+		printf("Parent of %d: %d\n\n",numArray[8], findParent(numArray[8],t->root)->key);
+		}
+
+	if (DELETE_WRITTEN == 1)	{
+		printf ("Tree after deleting %d:\n",numArray[3]);
+		withdraw (numArray[3],t->root,&n);
+		printTree (t->root);
+		printf ("\n\n");
+		//free (n);
+		printf("Tree after deleting %d:\n",numArray[6]);
+		withdraw(numArray[6],t->root,&n);
+		printTree(t->root);
+		printf("\n\n");
+		//free(n);
+
+	    printf("Tree after deleting %d:\n",numArray[8]);
+		withdraw(numArray[8],t->root,&n);
+		printTree(t->root);
+		printf("\n");
+		//free(n);
+		}
+	
+	return 0;
+}//main()
